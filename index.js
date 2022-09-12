6a33eb55d8b32407cbd6719f5a24fa876833d8ff
Maxim Polyakov <polyakovmaxim05@gmail.com>
2022-09-12 16:37:55 -0400

    Initiall Commit

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const navbarSection = document.querySelector('.navbar-section');
 const link = document.getElementsByClassName('link');
 const navbarBrend = document.querySelector('.navbar-brend');
-const closeLogInBtn = document.getElementById('btn-close');
-const logInWindow = document.getElementById('log-in');
-const logInSignInBtn = document.getElementById('log-in-sign-in-btn');
 
 window.addEventListener('scroll', () =>{
     let scrolled = Math.floor(window.scrollY);
@@ -21,14 +18,4 @@ window.addEventListener('scroll', () =>{
         }
     }
 })
-
-closeLogInBtn.addEventListener('click', () =>{
-    logInWindow.style.display = 'none';
-})
-
-logInSignInBtn.addEventListener('click', () =>{
-    logInWindow.style.display = 'block';
-})
-
-
 
