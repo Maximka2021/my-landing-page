61d528a5ea1d9585c84ed3333213814454f3c7c9
Maxim Poyakov <87499847+Maximka2021@users.noreply.github.com>
2022-09-16 12:46:48 -0400

    Add files via upload

---
+++ log-in.js
@@ -0,0 +1,7 @@
+const signUpBtn = document.getElementById('sign-up-btn');
+const password = document.getElementById('password');
+const username = document.getElementById('username');
+
+signUpBtn.addEventListener('click', (e) =>{
+    e.preventDefault();
+})
