0b072e067fa74334b68cc6d2ca255a0fbb59c0cd
Maxim Poyakov <87499847+Maximka2021@users.noreply.github.com>
2022-09-16 12:55:01 -0400

    Delete log-in.js

--- log-in.js
+++
@@ -1,7 +0,0 @@
-const signUpBtn = document.getElementById('sign-up-btn');
-const password = document.getElementById('password');
-const username = document.getElementById('username');
-
-signUpBtn.addEventListener('click', (e) =>{
-    e.preventDefault();
-})
